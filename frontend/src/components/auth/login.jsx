104b3bc7eacbc38d776f63e388801a371265fd92
arno <andriaarno@gmail.com>
2026-01-10 04:34:03 +0000

    connexion

--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -156,7 +156,7 @@ export default function Login() {
             className="text-4xl font-bold mb-6 text-center"
             style={{ color: "var(--text-primary)" }}
           >
-            page de connexion
+            connexion 
           </h1>
 
           {message && (
